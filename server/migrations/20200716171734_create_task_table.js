2ab4dd20c68bb54776fafaac48c662cf6fc8fe55
Тимур Хабибов <Tim-him@mail.ru>
2022-07-24 22:13:09 +0500

    fix

--- server/migrations/20200716171734_create_task_table.js
+++ server/migrations/20200716171734_create_task_table.js
@@ -1,24 +1,23 @@
 // @ts-check
 
 export const up = (knex) => knex.schema.createTable('tasks', (table) => {
-    table.increments('id').primary();
-    table.string('name');
-    table.string('description');
-    table.integer('status_id').unsigned()
-    table.foreign('status_id')
-      .references('id')
-      .inTable('task_statuses');
-    table.integer('creator_id').unsigned();
-    table.foreign('creator_id')
-      .references('id')
-      .inTable('users');
-    table.integer('executor_id').unsigned();
-    table.foreign('executor_id')
-      .references('id')
-      .inTable('users');
-    table.timestamp('created_at').defaultTo(knex.fn.now());
-    table.timestamp('updated_at').defaultTo(knex.fn.now());
-  }).then(() => console.log('tasks done'));
+  table.increments('id').primary();
+  table.string('name');
+  table.string('description');
+  table.integer('status_id').unsigned()
+  table.foreign('status_id')
+    .references('id')
+    .inTable('task_statuses');
+  table.integer('creator_id').unsigned();
+  table.foreign('creator_id')
+    .references('id')
+    .inTable('users');
+  table.integer('executor_id').unsigned();
+  table.foreign('executor_id')
+    .references('id')
+    .inTable('users');
+  table.timestamp('created_at').defaultTo(knex.fn.now());
+  table.timestamp('updated_at').defaultTo(knex.fn.now());
+}).then(() => console.log('tasks done'));
   
 export const down = (knex) => knex.schema.dropTable('tasks').then(() => console.log('drop tasks done!'));
-
